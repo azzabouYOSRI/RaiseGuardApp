85cf10f74a029e828f072ce2fef536b8dfca9c59
yosri azzabou <Yosriazzaboup9@gmail.com>
2025-03-29 06:16:00 +0100

    Add GitHub Actions CI config, SwiftLint, SwiftFormat, and Sonar setup

--- RaiseGuardAppTests/RaiseGuardAppTests.swift
+++ RaiseGuardAppTests/RaiseGuardAppTests.swift
@@ -5,13 +5,11 @@
 //  Created by yosri azabou on 29/3/2025.
 //
 
-import Testing
 @testable import RaiseGuardApp
+import Testing
 
 struct RaiseGuardAppTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
